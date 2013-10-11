281ed771d54eae8b159b81cffdb6fb63b9de948b
shuen-shiang-yang <brian19960@hotmail.com>
2013-10-11 05:27:05 -0700

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,26 +1,40 @@
 #include <stdio.h>
 #include <inttypes.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-#define READ_COUNTER_ADDR 0x40050000
 
-int32_t *read_counter = (int32_t *) READ_COUNTER_ADDR;
+//#define READ_COUNTER_ADDR 0x40050000
+//int32_t *read_counter = (int32_t *) READ_COUNTER_ADDR;
+
 int main(void)
 {
-	printf("This is a test program for QEMU counter device\n");
-	printf("See http://github.com/krasin/qemu-counter for more details\n\n");
-	printf("Let's check if the Read Counter device presented\n");
-	for (int i = 0; i < 10; i++) {
-		printf("The device has been accessed for %"PRId32" times!\n", *read_counter);
-	}
-	int32_t now = *read_counter;
-	if (now == 0) {
-		printf("ERROR - No Read Counter detected\n");
-	}
-	else if (now == 11) {
-		printf("OK - Read Counter works as intended\n");
-	}
-	else {
-		printf("ERROR - Something is wrong with Read Counter\n");
-	}
+	FILE* note;
+    	char str[100];
+	char buff[100];
+
+    	note=fopen("file_save.txt","w");
+    	if(note==NULL){
+        	printf("read error!\n\r");
+        	return 0;
+    	}
+	
+	printf("type the string you want leave:\n\r");
+	scanf("%s",str);
+	printf("the string already write into the note\n\r");
+    	fprintf(note,"%s",&str);
+    	fclose(note);
+
+
+    	note=fopen("file_save.txt", "r");
+    	if(note==NULL){
+        	printf("read error!\n\r");
+        	return 0;
+    	}
+
+    	fread(buff,sizeof(buff),1,note);
+	printf("\"%s\" is read from the note\n\r",buff);
+    	fclose(note);
+
 	return 0;
 }
